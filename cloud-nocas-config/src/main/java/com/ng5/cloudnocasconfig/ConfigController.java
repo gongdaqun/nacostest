0a158d14a50c492bd04263fc38dd9b2eccd7b541
龚大群 <gongdaqun@newgrand.cn>
2019-08-15 14:13:54 +0800

    nacostest demo2

--- cloud-nocas-config/src/main/java/com/ng5/cloudnocasconfig/ConfigController.java
+++ cloud-nocas-config/src/main/java/com/ng5/cloudnocasconfig/ConfigController.java
@@ -1,6 +1,5 @@
 package com.ng5.cloudnocasconfig;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
